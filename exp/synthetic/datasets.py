e830fdce3611b7a4aa62612cbfcb5c6b0012f799
ecreager <creager@cs.toronto.edu>
2018-09-19 13:18:25 -0400

    dataset and loader

--- exp/synthetic/datasets.py
+++ exp/synthetic/datasets.py
@@ -6,13 +6,14 @@ from torch.utils.data import Dataset
 
 class MixtureOfBlocks(Dataset):
     im_shape = (1, 28, 28)
-    def __init__(self, num_samples):
+    def __init__(self, num_samples, train=True):
         super(MixtureOfBlocks, self).__init__()
-        labels = np.random.choice(range(9), ((100, )))
+        labels = np.random.choice(range(9), ((num_samples, )))
         beta_samples = np.random.beta(5., 1., size=((num_samples, )))  # should probably seed the RNG here...
         samples = np.stack([self.generate_image(b, label) for b, label in zip(beta_samples, labels)], 0)
         self.num_samples = num_samples
         self.labels, self.samples = torch.Tensor(labels).unsqueeze(1), torch.Tensor(samples)
+        self.train = train  # for bookeeping purposes
 
     def generate_image(self, beta_sample, label):
         #n_pixels_per_edge = self.im_shape[1]
@@ -21,8 +22,9 @@ class MixtureOfBlocks(Dataset):
         stride = width // 2
         row_idx = label // n_blocks_per_edge
         col_idx = label % n_blocks_per_edge
-        im = np.zeros(self.im_shape)
-        im[0, row_idx*stride:row_idx*stride+width, col_idx*stride:col_idx*stride+width] = beta_sample 
+        #im = np.zeros(self.im_shape)  # noiseless
+        im = 0.2*np.random.randn(*self.im_shape)  # background noise
+        im[0, row_idx*stride:row_idx*stride+width, col_idx*stride:col_idx*stride+width] += beta_sample 
         return im
 
     def __len__(self):
@@ -40,6 +42,20 @@ def MaskedMixtureOfBlocks(num_samples):
         1/0
 
 
+def mixture_of_blocks(num_samples, batch_size, seed=None, 
+        use_cuda=torch.cuda.is_available()):
+    """get a mixture-of-blocks toy dataset loader"""
+    if seed is not None:
+        torch.manual_seed(seed)
+    loader_kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+    dataset_kwargs = dict()
+    train_loader = torch.utils.data.DataLoader(
+            MixtureOfBlocks(num_samples, train=True),
+            batch_size=batch_size, 
+            shuffle=True, 
+            **loader_kwargs)
+    return train_loader
+
 
 if __name__ == '__main__':
     import os
@@ -49,12 +65,14 @@ if __name__ == '__main__':
     if not os.path.exists(dirname):
         os.makedirs(dirname)
 
-    d = MixtureOfBlocks(100)
-    dl = iter(d)
-    for i in range(20):
-        x, y = next(dl)
+    num_samples = 1024
+    batch_size = 16
+    seed = 0
+
+    mob = mixture_of_blocks(num_samples, batch_size, seed)
+    for i, (x, y) in enumerate(mob):
         print(i, x.shape, torch.norm(x), y)
-        save_image(x, '{}/foo{}-y={}.png'.format(dirname, i, int(y)))
+        save_image(x, '{}/foo{}.png'.format(dirname, i), nrow=int(batch_size ** 0.5))
 
     print('done')
     1/0
